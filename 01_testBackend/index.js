0546722f6ae9622b0b0556c879238a6c6c43897d
Harshit Pradhan <pradhanharshit089@gmail.com>
2025-01-05 13:57:50 +0530

    test commit

---
+++ 01_testBackend/index.js
@@ -0,0 +1,21 @@
+// import express from 'express'
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/login', (req, res)=>{
+    res.send('Login world')
+})
+
+app.get('/bye', (req, res)=>{
+    res.send('bye world')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
